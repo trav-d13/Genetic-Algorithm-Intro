f385c661d981a86a2aa8c5a14da0ec9c6bc46464
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-07-13 10:17:34 +0200

    Reproduction occurs with a 90% probability. Capability to specify method of reproduction

--- Reproduction.py
+++ Reproduction.py
@@ -2,6 +2,21 @@ import random
 
 from Individual import Individual
 
+reproduce_prob = 0.9  # Probability of parents reproducing
+
+
+def reproduce(parent_1: Individual, parent_2: Individual, method: str):
+    prob = random.uniform(0, 1)
+
+    child_1 = parent_1  # Assign parents as children, in case reproduction does not occur
+    child_2 = parent_2
+
+    if prob < reproduce_prob:  # Determine if reproduction occurs
+        match method:  # Determine method of reproduction
+            case "Single":
+                child_1, child_2 = single_point_crossover(parent_1, parent_2)
+    return child_1, child_2
+
 
 def single_point_crossover(parent_1: Individual, parent_2: Individual):
     point = random.randint(a=0, b=len(parent_1.genetics))  # Determine random point of crossover in genetics
